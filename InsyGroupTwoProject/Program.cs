ffd7dcd01b43aaa2e524bb4fe707ae76723772c1
sarah <sarah@SARAH-PC>
2023-04-24 16:01:53 -0500

    Add project files.

---
+++ InsyGroupTwoProject/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace InsyGroupTwoProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
