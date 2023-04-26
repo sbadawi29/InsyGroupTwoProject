060bfdb350f3512e39417e25a0132a7af711d570
marclagler <marc.lagler@marquette.edu>
2023-04-26 16:17:05 -0500

    Created the customer Class

--- InsyGroupTwoProject/Model/Client.cs
+++ InsyGroupTwoProject/Model/Client.cs
@@ -3,9 +3,28 @@ namespace InsyGroupTwoProject.Model
 {
 	public class Client
 	{
-		public Client()
-		{
-		}
-	}
+
+        public Client()
+        {
+            autoIncreament++;
+            Id = autoIncreament;
+
+        }
+
+        private static int autoIncreament = 006000000;
+
+		public int Id { get; set; }
+
+        public string Username { get; set; }
+
+		public string Password { get; set; }
+
+		public string FirstName { get; set; }
+
+		public string LastName { get; set; }
+
+		
+
+    }
 }
 
--- InsyGroupTwoProject/Program.cs
+++ InsyGroupTwoProject/Program.cs
@@ -1,10 +1,12 @@
-﻿namespace InsyGroupTwoProject
+﻿using InsyGroupTwoProject.Model;
+
+namespace InsyGroupTwoProject
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!"); 
+            Console.WriteLine("Hello, World!");
         }
     }
 }
